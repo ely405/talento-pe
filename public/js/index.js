431b46b7b514fd0c07fc24972a5052fe17681f18
ely211 <gonzalesm405@gmail.com>
2017-06-20 19:39:34 -0500

    component welcome

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,26 @@
+
 'use strict';
 
 const update = ()=>{
-     render(documentRoot);
+  render(documentRoot);
+}
+const render = (documentRoot)=>{
+  documentRoot.empty();
+  documentRoot.append(createWelcome(update));
+}
+
+const state={
+  screen : null,
+  btnCheckTerms : false
+}
+
+const documentLoad = ()=>{
+  state.screen = 'welcome';
+  let rootToLoad = $('#root');
+  render(rootToLoad);
 }
 
 $(document).ready(function(){
+    documentLoad();
      $('.carousel').carousel();
 });
